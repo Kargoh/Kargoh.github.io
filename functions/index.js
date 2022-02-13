513a6298c4233bc9dbc08da6e38e3857e532eedf
kargoh <deben6@gmail.com>
2022-02-13 14:02:16 -0700

    further integration of stripe checkout

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,62 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+
+// This example sets up an endpoint using the Express framework.
+// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+
+exports.CheckoutSession = functions.https.onRequest(() => {
+        // server.js
+        //
+        // Use this sample code to handle webhook events in your integration.
+        //
+        // 1) Paste this code into a new file (server.js)
+        //
+        // 2) Install dependencies
+        //   npm install stripe
+        //   npm install express
+        //
+        // 3) Run the server on http://localhost:4242
+        //   node server.js
+
+        const stripe = require('stripe');
+        const express = require('express');
+        const app = express();
+
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        const endpointSecret = "whsec_69fff72d4679ee0f762325b1e1647004cfb4ff820e6012af6815b0df51a5a165";
+
+        app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+        const sig = request.headers['stripe-signature'];
+
+        let event;
+
+        try {
+            event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+        } catch (err) {
+            response.status(400).send(`Webhook Error: ${err.message}`);
+            return;
+        }
+
+        // Handle the event
+        switch (event.type) {
+            case 'payment_intent.succeeded':
+            const paymentIntent = event.data.object;
+            // Then define and call a function to handle the event payment_intent.succeeded
+            break;
+            // ... handle other event types
+            default:
+            console.log(`Unhandled event type ${event.type}`);
+        }
+
+        // Return a 200 response to acknowledge receipt of the event
+        response.send();
+        });
+
+        app.listen(5001, () => console.log('Running on port 3000'));
+})
--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <Navbar />
   <div class="checkout container">
     <h1>Checkout Page</h1>
@@ -18,7 +19,20 @@
         </ul>
       </div>
       <div class="col-3">
-        <h2>Total Price</h2>
+        <h2>Card Information</h2>
+
+        <form id="payment-form">
+          <div id="payment-element">
+            <!-- Elements will create form elements here -->
+            <label>Name on Card</label><input class="form-control mr-sm-2" type="text" placeholder="Name on Card" aria-label="name">
+            <label>Card Number</label><input class="form-control mr-sm-2" type="text" placeholder="Name on Card" aria-label="name">
+
+          </div>
+          <button type="submit" class="btn btn-outline-success my-2 my-sm-0" id="submit">Submit</button>
+          <div id="error-message">
+            <!-- Display error message to your customers here -->
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -26,8 +40,58 @@
 </template>
 
 <script>
+// import Stripe from "https://js.stripe.com/v3/"
 import Navbar from '/src/components/Navbar.vue'
 import Footer from '/src/components/Footer.vue'
+// const axios = require('axios')
+// // Set your secret key. Remember to switch to your live secret key in production.
+// // See your keys here: https://dashboard.stripe.com/apikeys
+// const stripe = Stripe('pk_test_51KSXKQFCPDEQnRi15eXSbGVe7SAQC2AM5c5GLHE8BjYDsiMUeAC4pL52iC3NQBhtclfHhPMAIskGZwnw5YcwfZIx00n6xde40v');
+
+// const paymentIntent = await stripe.paymentIntents.create({
+//   amount: 1099,
+//   currency: 'usd',
+//   automatic_payment_methods: {enabled: true},
+// });
+
+// const options = {
+//   clientSecret: 'sk_test_51KSXKQFCPDEQnRi1mEWq8Ph3UhFJiuvMD9Xn3zc7b9Iw0lrBQjAGjnGTAtsAVFyPjv431vexZhJlBOU5D9RaOdeg00Wz6nMdCh',
+//   // Fully customizable with appearance API.
+//   appearance: {/*...*/},
+// };
+
+// // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 2
+// const elements = stripe.elements(options);
+
+// // Create and mount the Payment Element
+// const paymentElement = elements.create('payment');
+// paymentElement.mount('#payment-element');
+
+// const form = document.getElementById('payment-form');
+
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const {error} = await stripe.confirmPayment({
+//     //`Elements` instance that was used to create the Payment Element
+//     elements,
+//     confirmParams: {
+//       return_url: 'https://my-site.com/order/123/complete',
+//     },
+//   });
+
+//   if (error) {
+//     // This point will only be reached if there is an immediate error when
+//     // confirming the payment. Show error to your customer (for example, payment
+//     // details incomplete)
+//     const messageContainer = document.querySelector('#error-message');
+//     messageContainer.textContent = error.message;
+//   } else {
+//     // Your customer will be redirected to your `return_url`. For some payment
+//     // methods like iDEAL, your customer will be redirected to an intermediate
+//     // site first to authorize the payment, then redirected to the `return_url`.
+//   }
+// });
 
 export default {
   name: 'about',
